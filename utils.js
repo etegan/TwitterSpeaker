7d794127b65d4b515324255753f751c89e300ea3
Eric Egan <egan1551@gmail.com>
2017-09-27 16:20:00 +0200

    task scheduling example in utils

--- utils.js
+++ utils.js
@@ -1,19 +1,26 @@
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
 var FormData = require('form-data');
 var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var spawn = require('child_process').spawn
 exports.createFileString = function(){
     var today = new Date();
-    return './tweets/' + today.toISOString().substr(0,10) 
+    return './tweets/' + createDateString(today); 
 };
 
 exports.createYesterdayFileString = function(){
     var today = new Date();
     var yesterday = today.setDate(today.getDate() - 1);
-    return './tweets/' + yesterday.toISOString().substr(0,10) 
+    return './tweets/' + createDateString(yesterday); 
 }
 
-exports.synthesizeText= function(text, func){
+exports.scheduleTextReading = function(text, time){
+    var dir = mp3Path();
+    mkdirp(dir);
+    synthesizeText(text, path.resolve(dir, time+".mp3"));
+}
+
+function synthesizeText(text, path){
     var data = new FormData()
     data.append('cl_login', process.env.ACAPELA_CL_LOGIN)
     data.append( 'cl_app', process.env.ACAPELA_CL_APP)
@@ -21,12 +28,23 @@ exports.synthesizeText= function(text, func){
     data.append('req_voice', 'elin22k');
     data.append( 'req_text', text)
     data.append('req_asw_type', "SOUND");
-    data.submit('http://vaas.acapela-group.com/Services/Synthesizer', func);
+    data.submit('http://vaas.acapela-group.com/Services/Synthesizer', function(err, res){
+        var stream = fs.createWriteStream(path);
+        res.pipe(stream);
+    })
 }
 
+function mp3Path(){
+    return path.resolve('mp3', createDateString(new Date()));
+}
 
-exports.synthesizeText("Här har vi lite testtext", function(err, res){
-    var stream = fs.createWriteStream("test.mp3");
-    res.pipe(stream);
+function createDateString(date){
+    return date.toISOString().substr(0, 10);
+}
+
+var at = spawn('at',['1609']);
+at.stdin.write('mpg123 test.mp3\n');
+at.stdin.end();
+at.on('close', function(code){
+    console.log(`Exited with code: ${code}`);
 })
-
